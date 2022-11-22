b9b7f3407c917d1d4b815e21a3355d0d1719c9a1
Victoria Godsoe <vgodsoe@groq.com>
2022-11-22 14:17:08 -0800

    Updating Export File

--- groqflow/justgroqit/export.py
+++ groqflow/justgroqit/export.py
@@ -527,9 +527,7 @@ class ConvertOnnxToFp16(stage.GroqitStage):
         # Legalize ops are ops that have been or are currently in the block list
         # that we explicitly want removed
         legalize_ops = ["InstanceNormalization", "Resize", "Max"]
-        op_block_list = (
-            onnxmltools.utils.float16_converter.DEFAULT_OP_BLOCK_LIST.copy()
-        )
+        op_block_list = onnxmltools.utils.float16_converter.DEFAULT_OP_BLOCK_LIST.copy()
         for op in legalize_ops:
             # Check to see that they are not in the block list before we remove them
             # Neccesary because the block list may be updated, and not in the state we expect
